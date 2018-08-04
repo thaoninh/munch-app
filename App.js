3d058577acd5faa0e7ea5779b5eefe6cf4795e2e
thaoninh.email@gmail.com <thaoninh.email@gmail.com>
2018-08-04 14:51:58 -0700

    Upload logo

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <Image style={{width: 200, height: 200}} source={require('./res/munch.png')} />
       </View>
     );
   }
@@ -18,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
